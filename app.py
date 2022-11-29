b1b483a520bfd97f0a48641189b8bf58fa4bb3da
vikranttdev <118067552+vikranttdev@users.noreply.github.com>
2022-11-29 15:08:37 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from _datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    desc = db.Column(db.String(500), unique=True, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow, unique=True )
+
+    def __repr__(self):
+        return f"{self.sno}--{self.title}"
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method =='POST':
+        title=request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    alltodo = Todo.query.all()
+    return render_template('index.html',alltodo=alltodo)
+
+
+@app.route('/update/<int:sno>',methods=['GET','POST'])
+def update(sno):
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__=="__main__":
+    app.run(debug=True,port=8000)
